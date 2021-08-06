0199de2b90dab3d2255a73b5d45a8ddab69f240f
karen2719 <hiahmadyan@gmail.com>
2021-08-06 23:16:33 +0430

    added IndexView

--- basicapp/views.py
+++ basicapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LoginView():
+    pass
+
+
+class SignUpView():
+    pass
+
+
+class IndexView(TemplateView):
+    template_name = 'basicapp/index.html'
--- to_do_app_v1/urls.py
+++ to_do_app_v1/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from basicapp.views import LoginView, SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('', include('basicapp.urls')),
 ]
